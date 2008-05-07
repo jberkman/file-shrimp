ea5e0dc98e3b95ef6dd814e6766d32d4c158bc5a
jacob berkman <jacob@87k.net>
2008-05-07 18:54:59 +0000

    initial import
    git-svn-id: http://loolixbodes.googlecode.com/svn/trunk@36 aa23fe99-163d-0410-8ed0-b94f67433ec0

---
+++ callbacks.c
@@ -0,0 +1,18 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_drives_combo_changed                (GtkComboBox     *combobox,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ callbacks.h
@@ -0,0 +1,6 @@
+#include <gtk/gtk.h>
+
+
+void
+on_drives_combo_changed                (GtkComboBox     *combobox,
+                                        gpointer         user_data);
